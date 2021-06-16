3c75160bd2e156713836d895b2be963562a4d2fe
animaliads-team <ds.animalia@gmail.com>
2021-06-16 09:19:18 -0300

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import '@animaliads/ani-button/index.js';
+import '@animaliads/ani-button';
 import '@animaliads/css-tokens/global-tokens.css';
+import '@animaliads/animalia-brand/theme.css';
 
 export default function App() {
   return <ani-button>Hello React!</ani-button>;
